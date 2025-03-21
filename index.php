d29a4e9acc658431f27a20804ec6866f4e6cc4db
Daniel <debem1972@gmail.com>
2025-03-21 16:52:46 -0300

    Foi criado o meu primeiro arquivo php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 de PHP</title>
+</head>
+<body>
+   <?php 
+   echo "<h1>Olá mundo! Mei primeiro contato com php!</h1>"
+   ?>
+</body>
+</html>
